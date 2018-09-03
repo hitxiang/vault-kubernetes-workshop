c5d80bd03f92547df6702b2163174c61a8fab51a
Zhang Mingxiang <zack_zhang@invastsec.jp>
2018-09-03 11:01:33 +0900

    need to enable alpha features of cluster to shareProcessNamespace

--- scripts/13-create-another-cluster.sh
+++ scripts/13-create-another-cluster.sh
@@ -9,11 +9,13 @@ fi
 ZONE="us-west1-b"
 CLUSTER_VERSION="1.10.6-gke.2"
 # Create a cluster with alpha features so we can do process namespace sharing
+# shareProcessNamespace: true
 gcloud container clusters create my-apps \
   --cluster-version ${CLUSTER_VERSION} \
   --enable-cloud-logging \
   --enable-cloud-monitoring \
   --machine-type custom-1-1536 \
+  --enable-kubernetes-alpha \
   --num-nodes 3 \
   --scopes "cloud-platform" \
   --zone "${ZONE}"
