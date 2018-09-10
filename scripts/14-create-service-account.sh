645d44b8b27715e02f3e3543e375d9a35e2d95ee
Zhang Mingxiang <zack_zhang@invastsec.jp>
2018-09-10 16:24:00 +0900

    remove namespace from clusterrole binding

--- scripts/14-create-service-account.sh
+++ scripts/14-create-service-account.sh
@@ -6,11 +6,10 @@ kubectl apply -f k8s/app_namespace.yaml
 kubectl create serviceaccount vault-auth --namespace zack-app
 kubectl apply -f - <<EOH
 ---
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
   name: role-tokenreview-binding
-  namespace: zack-app
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
